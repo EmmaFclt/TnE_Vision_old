b9cb2d5d32c85e78e588a2639f44891031517801
Paula <paula.beltran.pb@gmail.com>
2020-02-01 13:47:36 +0100

    add methodes to cont

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,27 @@
 class ReportsController < ApplicationController
 
   def new
+    @report = Report.new
   end
 
   def create
+    @report = Report.new(report_params)
+    @report.user = current_user
   end
 
   def index
+    #put filters for each KPI and/or add option search
   end
 
   def destroy
+    @report = Report.find(params[:id])
+    @report.destroy
+  end
+
+
+  private
+
+  def report_params
+    params.require(:report).permit(:format, :submission_date, :source)
   end
 end
