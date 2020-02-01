72a9a7bf03a32cf638c0e1e90e3f79881311f20e
Emma Foucault <foucault.emma@gmail.com>
2020-02-01 15:15:31 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'devise/sessions#new'
+  root to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :reports, only: %i[new, create, index, destroy]
-  get 'dashboard', to: 'pages#dashboard', as: :dashboard
-
 end
